ffc4122bddbc792e1617d879f0b3b0565453a14d
Georgi Nikolov <Nikolov.georgi18@gmail.com>
2022-01-20 21:30:15 -0600

    new files

---
+++ sketch.js
@@ -0,0 +1,27 @@
+// Georgi Nikolov
+// Solar System Model
+// "sketch.js"
+
+let sun
+
+function setup() {  // Setting up the main "window"
+    createCanvas(700,700)
+    sun = new Body(100, createVector(0,0), createVector(0,0))
+}
+
+function draw() {   // background color & planets
+    background(180)
+    sun.show()
+}
+
+function Body(_mass, _pos, _vel){
+    this.mass = _mass
+    this.pos = _pos
+    this.vel = _vel
+    this.r = this.mass
+
+    this.show = function(){
+        noStroke(); fill(255); 
+        ellipse(this.pos.x, this.pos.y, this.r, this.r)
+    }
+}
